48e9405e7c9f25c5078b3a148973e93dfd872836
osama sameh <oo215881@gmail.com>
2026-04-02 23:31:19 +0200

    about section

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Hero } from "./componants/hero/hero";
+import { AboutUs } from './componants/about-us/about-us';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hero],
+  imports: [RouterOutlet, Hero,AboutUs],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
